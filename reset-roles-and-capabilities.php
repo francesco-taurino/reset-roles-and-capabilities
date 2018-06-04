e8d6bbb286d5a54f288c66ec5d44c2220147c755
francesco-taurino <32375156+francesco-taurino@users.noreply.github.com>
2018-06-04 23:29:24 +0200

    Update reset-roles-and-capabilities.php

--- reset-roles-and-capabilities.php
+++ reset-roles-and-capabilities.php
@@ -1,77 +1,63 @@
 <?php
 /*
- * Plugin Name: Reset Roles and Capabilities to WordPress defaults.  
+ * Plugin Name: Reset Roles and Capabilities  
  * Plugin URI: https://francescotaurino.com
- * Description: Reset Roles and Capabilities to WordPress defaults. 
+ * Description: Reset Roles and Capabilities to WordPress defaults
  * Author: Francesco Taurino
- * Version: 0.1
- * Text Domain: rrac
  * Domain Path: languages
  * Author URI: https://www.francescotaurino.com
- * Compatibility: WordPress 2.0.0
+ * License: GPL v2 or later
+ *
+ * Version: 1.0
+ * Requires PHP: 5.2
+ * Requires at least: 2.8.0
+ * Tested up to: 4.9.6
  */
+
 /*
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 
+	2 of the License, or (at your option) any later version.
+	
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+	
+	You should have received a copy of the GNU General Public License
+	with this program. If not, visit: https://www.gnu.org/licenses/
 
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+	Copyright 2018 Monzilla Media. All rights reserved.
 */
 
 
-/**
-* add_action('init', 'ResetRolesAndCapabilitiesToWordPressDefault' );
-*/
-function ResetRolesAndCapabilitiesToWordPressDefault(){
+define('RRAC_TRANSIENT_TIME', 5 );
+define('RRAC_TRANSIENT_ERROR','rrac-failed');
+define('RRAC_TRANSIENT_SUCCESS','rrac-success');
+
+
+register_activation_hook( __FILE__, function () {
+
+	global $wp_roles;
 
   require_once( ABSPATH . 'wp-admin/includes/schema.php' );
+  
   if ( !function_exists( 'populate_roles' ) ) {
+  	// Error
+  	set_transient( RRAC_TRANSIENT_ERROR, 'RRAC: populate_roles() is required', RRAC_TRANSIENT_TIME );
     return false;
   }
-
-  if ( !function_exists( 'wp_roles' ) ) {
-    return false;
-  }
-
-  if ( !function_exists( 'get_role' ) ) {
-    return false;
-  }
-
-  global $wp_roles;
 
   if ( ! isset( $wp_roles ) ) {
 
-    /**
-     * WP_Roles
-     * @since Wp 2.0.0
-     */
     $wp_roles = new WP_Roles();
   }
 
-  /**
-   * Fetch roles
-   * 
-   * Alternative: @ since Wp 4.3.0 $all_roles = wp_roles()->roles;
-   * Alternative: @ since Wp 2.8.0 $all_roles = get_editable_roles();
-   */
-  $all_roles = $wp_roles->roles;
-  
-  foreach ( $all_roles as $role_name => $role_info ): 
-
+  foreach ( $wp_roles->roles as $role_name => $role_info ): 
 
     /**
      * Fetch a Role definition.
-     * get_role returns an instance of WP_Role. 
-     * https://codex.wordpress.org/Function_Reference/get_role
      */
     $role = get_role( $role_name );
 
@@ -79,7 +65,6 @@ function ResetRolesAndCapabilitiesToWordPressDefault(){
 
       /**
        * Removes a capability from a role
-       * https://codex.wordpress.org/Function_Reference/remove_cap
        */
       $role->remove_cap( $capability );
 
@@ -87,17 +72,43 @@ function ResetRolesAndCapabilitiesToWordPressDefault(){
 
     /**
      * Remove role, if it exists.
-     * @since Wp 2.0.0
      */
     remove_role($role_name);
 
   endforeach;
 
-
   /**
    * Execute WordPress role creation for the various WordPress versions.
-   * @since Wp 2.0.0
    */
   populate_roles();
   
-}
+  // Success
+  set_transient( RRAC_TRANSIENT_SUCCESS, __('RRAC: Plugin deactivated. Roles and Capabilities reset to WordPress defaults.', 'rrac'), RRAC_TRANSIENT_TIME );
+
+});
+
+
+/**
+ * Show a Message
+ */
+add_action( 'admin_notices', function () {
+
+	if( $text = get_transient( 	RRAC_TRANSIENT_ERROR ) ){
+	 	$class = 'notice notice-error is-dismissible';
+		printf( '<div class="%1$s"><p>%2$s</p></div>', esc_attr( $class ), esc_html( $text ) ); 
+	  delete_transient( RRAC_TRANSIENT_ERROR );
+	} elseif( $text = get_transient( 	RRAC_TRANSIENT_SUCCESS ) ){
+	 	$class = 'notice notice-success is-dismissible';
+		printf( '<div class="%1$s"><p>%2$s</p></div>', esc_attr( $class ), esc_html( $text ) ); 
+	  delete_transient( RRAC_TRANSIENT_SUCCESS );
+	}
+
+} );
+
+
+/**
+ * Deactivate
+ */
+add_action( 'admin_init', function () { 
+	deactivate_plugins( plugin_basename( __FILE__ ) ); 
+},0 );
